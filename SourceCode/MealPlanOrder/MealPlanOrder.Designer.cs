5bddc5c554fe180b1895cf023bf5385298ec4dc7
Nvi mai <nvimai2703@gmail.com>
2018-11-13 12:44:11 -0500

    Source Code + Flowchart + Requirements

---
+++ SourceCode/MealPlanOrder/MealPlanOrder.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace MealPlanOrder
+{
+    partial class MealPlanOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOrderNow = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbbBreakfast = new System.Windows.Forms.ComboBox();
+            this.cbbLunch = new System.Windows.Forms.ComboBox();
+            this.cbbDinner = new System.Windows.Forms.ComboBox();
+            this.txtBreakfastQuantity = new System.Windows.Forms.TextBox();
+            this.txtLunchQuantity = new System.Windows.Forms.TextBox();
+            this.txtDinnerQuantity = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.lblTax = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblBreakfastQuanReport = new System.Windows.Forms.Label();
+            this.lblLunchQuanReport = new System.Windows.Forms.Label();
+            this.lblDinnerQuanReport = new System.Windows.Forms.Label();
+            this.lblDinnerTypeReport = new System.Windows.Forms.Label();
+            this.lblLunchTypeReport = new System.Windows.Forms.Label();
+            this.lblBreakfastTypeReport = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOrderNow
+            // 
+            this.btnOrderNow.Location = new System.Drawing.Point(207, 496);
+            this.btnOrderNow.Name = "btnOrderNow";
+            this.btnOrderNow.Size = new System.Drawing.Size(159, 45);
+            this.btnOrderNow.TabIndex = 0;
+            this.btnOrderNow.Text = "Order Now";
+            this.btnOrderNow.UseVisualStyleBackColor = true;
+            this.btnOrderNow.Click += new System.EventHandler(this.btnOrderNow_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Breakfast";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Lunch";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Dinner";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(198, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Type";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(511, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Quantity";
+            // 
+            // cbbBreakfast
+            // 
+            this.cbbBreakfast.FormattingEnabled = true;
+            this.cbbBreakfast.Location = new System.Drawing.Point(201, 53);
+            this.cbbBreakfast.Name = "cbbBreakfast";
+            this.cbbBreakfast.Size = new System.Drawing.Size(309, 33);
+            this.cbbBreakfast.TabIndex = 6;
+            // 
+            // cbbLunch
+            // 
+            this.cbbLunch.FormattingEnabled = true;
+            this.cbbLunch.Location = new System.Drawing.Point(201, 140);
+            this.cbbLunch.Name = "cbbLunch";
+            this.cbbLunch.Size = new System.Drawing.Size(309, 33);
+            this.cbbLunch.TabIndex = 7;
+            // 
+            // cbbDinner
+            // 
+            this.cbbDinner.FormattingEnabled = true;
+            this.cbbDinner.Location = new System.Drawing.Point(201, 231);
+            this.cbbDinner.Name = "cbbDinner";
+            this.cbbDinner.Size = new System.Drawing.Size(309, 33);
+            this.cbbDinner.TabIndex = 8;
+            // 
+            // txtBreakfastQuantity
+            // 
+            this.txtBreakfastQuantity.Location = new System.Drawing.Point(516, 54);
+            this.txtBreakfastQuantity.Name = "txtBreakfastQuantity";
+            this.txtBreakfastQuantity.Size = new System.Drawing.Size(87, 31);
+            this.txtBreakfastQuantity.TabIndex = 9;
+            // 
+            // txtLunchQuantity
+            // 
+            this.txtLunchQuantity.Location = new System.Drawing.Point(516, 140);
+            this.txtLunchQuantity.Name = "txtLunchQuantity";
+            this.txtLunchQuantity.Size = new System.Drawing.Size(87, 31);
+            this.txtLunchQuantity.TabIndex = 10;
+            // 
+            // txtDinnerQuantity
+            // 
+            this.txtDinnerQuantity.Location = new System.Drawing.Point(516, 231);
+            this.txtDinnerQuantity.Name = "txtDinnerQuantity";
+            this.txtDinnerQuantity.Size = new System.Drawing.Size(87, 31);
+            this.txtDinnerQuantity.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(67, 356);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Subtotal";
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Location = new System.Drawing.Point(164, 356);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(0, 25);
+            this.lblSubtotal.TabIndex = 13;
+            // 
+            // lblTax
+            // 
+            this.lblTax.AutoSize = true;
+            this.lblTax.Location = new System.Drawing.Point(164, 399);
+            this.lblTax.Name = "lblTax";
+            this.lblTax.Size = new System.Drawing.Size(0, 25);
+            this.lblTax.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(67, 399);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 25);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Tax";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(164, 441);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 25);
+            this.lblTotal.TabIndex = 17;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(67, 441);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(60, 25);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Total";
+            // 
+            // lblBreakfastQuanReport
+            // 
+            this.lblBreakfastQuanReport.AutoSize = true;
+            this.lblBreakfastQuanReport.Location = new System.Drawing.Point(511, 97);
+            this.lblBreakfastQuanReport.Name = "lblBreakfastQuanReport";
+            this.lblBreakfastQuanReport.Size = new System.Drawing.Size(0, 25);
+            this.lblBreakfastQuanReport.TabIndex = 18;
+            // 
+            // lblLunchQuanReport
+            // 
+            this.lblLunchQuanReport.AutoSize = true;
+            this.lblLunchQuanReport.Location = new System.Drawing.Point(511, 187);
+            this.lblLunchQuanReport.Name = "lblLunchQuanReport";
+            this.lblLunchQuanReport.Size = new System.Drawing.Size(0, 25);
+            this.lblLunchQuanReport.TabIndex = 19;
+            // 
+            // lblDinnerQuanReport
+            // 
+            this.lblDinnerQuanReport.AutoSize = true;
+            this.lblDinnerQuanReport.Location = new System.Drawing.Point(511, 279);
+            this.lblDinnerQuanReport.Name = "lblDinnerQuanReport";
+            this.lblDinnerQuanReport.Size = new System.Drawing.Size(0, 25);
+            this.lblDinnerQuanReport.TabIndex = 20;
+            // 
+            // lblDinnerTypeReport
+            // 
+            this.lblDinnerTypeReport.AutoSize = true;
+            this.lblDinnerTypeReport.Location = new System.Drawing.Point(202, 279);
+            this.lblDinnerTypeReport.Name = "lblDinnerTypeReport";
+            this.lblDinnerTypeReport.Size = new System.Drawing.Size(0, 25);
+            this.lblDinnerTypeReport.TabIndex = 23;
+            // 
+            // lblLunchTypeReport
+            // 
+            this.lblLunchTypeReport.AutoSize = true;
+            this.lblLunchTypeReport.Location = new System.Drawing.Point(202, 187);
+            this.lblLunchTypeReport.Name = "lblLunchTypeReport";
+            this.lblLunchTypeReport.Size = new System.Drawing.Size(0, 25);
+            this.lblLunchTypeReport.TabIndex = 22;
+            // 
+            // lblBreakfastTypeReport
+            // 
+            this.lblBreakfastTypeReport.AutoSize = true;
+            this.lblBreakfastTypeReport.Location = new System.Drawing.Point(202, 97);
+            this.lblBreakfastTypeReport.Name = "lblBreakfastTypeReport";
+            this.lblBreakfastTypeReport.Size = new System.Drawing.Size(0, 25);
+            this.lblBreakfastTypeReport.TabIndex = 21;
+            // 
+            // MealPlanOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 560);
+            this.Controls.Add(this.lblDinnerTypeReport);
+            this.Controls.Add(this.lblLunchTypeReport);
+            this.Controls.Add(this.lblBreakfastTypeReport);
+            this.Controls.Add(this.lblDinnerQuanReport);
+            this.Controls.Add(this.lblLunchQuanReport);
+            this.Controls.Add(this.lblBreakfastQuanReport);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblTax);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDinnerQuantity);
+            this.Controls.Add(this.txtLunchQuantity);
+            this.Controls.Add(this.txtBreakfastQuantity);
+            this.Controls.Add(this.cbbDinner);
+            this.Controls.Add(this.cbbLunch);
+            this.Controls.Add(this.cbbBreakfast);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOrderNow);
+            this.Name = "MealPlanOrder";
+            this.Text = "Meal Plan Order";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOrderNow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbbBreakfast;
+        private System.Windows.Forms.ComboBox cbbLunch;
+        private System.Windows.Forms.ComboBox cbbDinner;
+        private System.Windows.Forms.TextBox txtBreakfastQuantity;
+        private System.Windows.Forms.TextBox txtLunchQuantity;
+        private System.Windows.Forms.TextBox txtDinnerQuantity;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label lblTax;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblBreakfastQuanReport;
+        private System.Windows.Forms.Label lblLunchQuanReport;
+        private System.Windows.Forms.Label lblDinnerQuanReport;
+        private System.Windows.Forms.Label lblDinnerTypeReport;
+        private System.Windows.Forms.Label lblLunchTypeReport;
+        private System.Windows.Forms.Label lblBreakfastTypeReport;
+    }
+}
+
---
+++ SourceCode/MealPlanOrder/MealPlanOrder.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace MealPlanOrder
+{
+    public partial class MealPlanOrder : Form
+    {
+        //The format currency
+        static CultureInfo cultureInfo = new CultureInfo("en-CA");
+
+        //The object class for the dish
+        public class DishInfo
+        {
+            public string name { get; set; }
+            public decimal prise { get; set; }
+            public string type { get; set; }
+            public void inputInfo(string name, decimal prise)
+            {
+                this.name = name;
+                this.prise = prise;
+            }
+
+            public string outputInfo()
+            {
+                return name + " - " + string.Format(cultureInfo, "{0:C}", prise);
+            }
+        }
+
+        // List kind of meal
+        public List<string> kindOfMeal = new List<string>() { "breakfast", "lunch", "dinner" };
+
+        //Tax
+        public static decimal tax = 0;
+        
+        // The list of breakfast meal
+        public List<DishInfo> breakfastMealList = new List<DishInfo>();
+        
+        // The list of lunch meal
+        public List<DishInfo> lunchMealList = new List<DishInfo>();
+
+        // The list of dinner meal
+        public List<DishInfo> dinnerMealList = new List<DishInfo>();
+
+        public MealPlanOrder()
+        {
+            InitializeComponent();
+            setMenu();
+        }
+
+        //Set tax to 13%
+        public void taxSetting()
+        {
+            tax = 13;
+        }
+        
+        //Set the menu
+        public void setMenu()
+        {
+            // Breakfast
+            breakfastMealList.Add(new DishInfo() { name = "Bagel", prise = 3.95M , type = kindOfMeal[0] });
+            breakfastMealList.Add(new DishInfo() { name = "Vegetarian Special", prise = 10.95M, type = kindOfMeal[0] });
+            breakfastMealList.Add(new DishInfo() { name = "Protein Platter", prise = 11.95M, type = kindOfMeal[0] });
+
+            // Display the menu into the list
+            foreach (var temp in breakfastMealList)
+            {
+                cbbBreakfast.Items.Add(temp.outputInfo());
+            }
+
+            // Lunch
+            lunchMealList.Add(new DishInfo() { name = "Bagel", prise = 3.95M, type = kindOfMeal[1] });
+            lunchMealList.Add(new DishInfo() { name = "Vegetarian Special", prise = 10.95M, type = kindOfMeal[1] });
+            lunchMealList.Add(new DishInfo() { name = "Protein Platter", prise = 11.95M, type = kindOfMeal[1] });
+
+            // Display the menu into the list
+            foreach (var temp in lunchMealList)
+            {
+                cbbLunch.Items.Add(temp.outputInfo());
+            }
+
+            // Dinner
+            dinnerMealList.Add(new DishInfo() { name = "Bagel", prise = 3.95M, type = kindOfMeal[2] });
+            dinnerMealList.Add(new DishInfo() { name = "Vegetarian Special", prise = 10.95M, type = kindOfMeal[2] });
+            dinnerMealList.Add(new DishInfo() { name = "Protein Platter", prise = 11.95M, type = kindOfMeal[2] });
+
+            // Display the menu into the list
+            foreach (var temp in dinnerMealList)
+            {
+                cbbDinner.Items.Add(temp.outputInfo());
+            }
+        }
+
+        // validate all user inputs to ensure all meals have a selected option (check that the SelectedIndex property of each ComboBox is greater than -1)
+        private bool IsSelected()
+        {
+            if(cbbBreakfast.SelectedIndex > -1 && cbbLunch.SelectedIndex > -1 && cbbDinner.SelectedIndex > -1)
+                return true;
+            return false;
+        }
+
+        //Numeric checking
+        public static bool IsNumber(string stringTemp)
+        {
+            try
+            {
+                decimal temp = Convert.ToDecimal(stringTemp);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        private void btnOrderNow_Click(object sender, EventArgs e)
+        {
+            // Get the list of meals from guest by the order
+            List<DishInfo> guestOrderList = new List<DishInfo>();
+            guestOrderList.Clear();
+
+
+            lblBreakfastTypeReport.Text = "";
+            if (cbbBreakfast.SelectedIndex < 0)
+            {
+                lblBreakfastTypeReport.Text = "Invalid Meal Selection.";
+                lblBreakfastTypeReport.ForeColor = Color.Red;
+                return;
+            }
+            guestOrderList.Add(breakfastMealList[cbbBreakfast.SelectedIndex]);
+
+            lblLunchTypeReport.Text = "";
+            if (cbbLunch.SelectedIndex < 0)
+            {
+                lblLunchTypeReport.Text = "Invalid Meal Selection.";
+                lblLunchTypeReport.ForeColor = Color.Red;
+                return;
+            }
+            guestOrderList.Add(lunchMealList[cbbLunch.SelectedIndex]);
+
+            lblDinnerTypeReport.Text = "";
+            if (cbbDinner.SelectedIndex < 0)
+            {
+                lblDinnerTypeReport.Text = "Invalid Meal Selection.";
+                lblDinnerTypeReport.ForeColor = Color.Red;
+                return;
+            }
+            guestOrderList.Add(dinnerMealList[cbbDinner.SelectedIndex]);
+
+            // Check number of the Breakfast Quantity
+            lblBreakfastQuanReport.Text = "";
+            if (!IsNumber(txtBreakfastQuantity.Text))
+            {
+                lblBreakfastQuanReport.Text = "Not a number!";
+                lblBreakfastQuanReport.ForeColor = Color.Red;
+                return;
+            }
+            decimal breakfastQuantity = Convert.ToDecimal(txtBreakfastQuantity.Text);
+
+            // Check number of the Lunch Quantity
+            lblLunchQuanReport.Text = "";
+            if (!IsNumber(txtLunchQuantity.Text))
+            {
+                lblLunchQuanReport.Text = "Not a number!";
+                lblLunchQuanReport.ForeColor = Color.Red;
+                return;
+            }
+            decimal lunchQuantity = Convert.ToDecimal(txtLunchQuantity.Text);
+
+            // Check number of the Dinner Quantity
+            lblDinnerQuanReport.Text = "";
+            if (!IsNumber(txtDinnerQuantity.Text))
+            {
+                lblDinnerQuanReport.Text = "Not a number!";
+                lblDinnerQuanReport.ForeColor = Color.Red;
+                return;
+            }
+            decimal dinnerQuantity = Convert.ToDecimal(txtDinnerQuantity.Text);
+
+            // Calculate the subTotal
+            decimal subTotal = breakfastQuantity * guestOrderList[0].prise + lunchQuantity * guestOrderList[1].prise + dinnerQuantity * guestOrderList[2].prise;
+            lblSubtotal.Text = string.Format(cultureInfo, "{0:C}",subTotal);
+
+            // Set tax
+            taxSetting();
+            lblTax.Text = Convert.ToString(tax) + "%";
+
+            // Calculate the total
+            decimal total = subTotal * (1 + tax / 100);
+            lblTotal.Text = string.Format(cultureInfo, "{0:C}", total);
+                
+            
+        }
+        
+    }
+}
